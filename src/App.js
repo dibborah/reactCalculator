9a7bae08acc66e3c43a206a18d913aa33bf57ab2
dibborah <dibyajyotiborah469@gmail.com>
2023-10-30 19:38:01 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import './style.css'
+import { useReducer } from 'react'
+
+export const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPEERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+}
+
+const reducer = (state, { type, payload }) => {
+  switch(type){
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${currentOperand || ""}${payload.digit}`
+      };
+  }
+}
+
+const App = () => {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer,{})
+
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className='span-two'>=</button>
+    </div>
+  )
+}
+
+export default App
